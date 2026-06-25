a222307cabf52549668faf8d3b4e7b6332837a96
RyanSweeney987 <ryan_sweeney@outlook.ie>
2026-06-25 14:04:15 +0100

    Imitial code commit

---
+++ tut-009-immediate-shader/Source/UE5_Tut_9_Immediate_Shader/Private/UE5_Tut_9_Immediate_Shader.cpp
@@ -0,0 +1,20 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE5_Tut_9_Immediate_Shader.h"
+
+#define LOCTEXT_NAMESPACE "FUE5_Tut_9_Immediate_ShaderModule"
+
+void FUE5_Tut_9_Immediate_ShaderModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FUE5_Tut_9_Immediate_ShaderModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FUE5_Tut_9_Immediate_ShaderModule, UE5_Tut_9_Immediate_Shader)
