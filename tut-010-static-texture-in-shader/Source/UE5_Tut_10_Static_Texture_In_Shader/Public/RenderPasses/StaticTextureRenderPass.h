e338ef96777ddceda21f8f31f4fbe7ae633170c1
RyanSweeney987 <ryan_sweeney@outlook.ie>
2026-07-10 10:24:33 +0100

    Scaffolding

---
+++ tut-010-static-texture-in-shader/Source/UE5_Tut_10_Static_Texture_In_Shader/Public/RenderPasses/StaticTextureRenderPass.h
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class UE5_TUT_10_STATIC_TEXTURE_IN_SHADER_API StaticTexture
+{
+public:
+	StaticTexture();
+	~StaticTexture();
+};
